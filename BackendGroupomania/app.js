8fd455f804b123f996e39647b6e63c152d029565
Gautier-c <gautier.couturier@gmail.com>
2020-09-23 10:03:06 +0200

    MAJ fichier app.js

--- BackendGroupomania/app.js
+++ BackendGroupomania/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
+const messagesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
+app.use(bodyparser.json());
+app.use('/api/messages', messagesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- BackendGroupomania/controllers/user.js
+++ BackendGroupomania/controllers/user.js
@@ -1,9 +1,26 @@
+const mysql = require('mysql');
 const User = require('../models/User'); //Appel du modele User
 
-exports.signup = (req, res, next) => {  
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "gautier",
+    password: "esther07",
+    database: "groupomaniaDB" 
+})
 
+exports.signup = (req, res, next) => {  
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        //Insert a record in the "customers" table:
+        var sql = "INSERT INTO customers (name, address) VALUES (req.body.email, req.body.password, req.body.name, req.body.firstname)";
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        });
+    });
 };
 
 exports.login = (req, res, next) => {
-    
+
 };
