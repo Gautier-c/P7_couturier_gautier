2b04a8e3eafcb2002302e82d056ccaf7fef1ca95
Gautier-c <gautier.couturier@gmail.com>
2020-09-23 10:39:18 +0200

    correction erreur routes messages.js

--- BackendGroupomania/routes/messages.js
+++ BackendGroupomania/routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();      
-const messageCtrl = require('../controllers/sauces');
+const messageCtrl = require('../controllers/messages');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
