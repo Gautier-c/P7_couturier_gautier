368d910caa78093587c0ec9c77d00819a3e5d9e6
Gautier-c <gautier.couturier@gmail.com>
2020-09-28 10:02:41 +0200

    Controllers users SignUp

--- BackendGroupomania/controllers/user.js
+++ BackendGroupomania/controllers/user.js
@@ -1,7 +1,14 @@
 const connect = require('../mysqlDbConnect');
 
-exports.signup = (req, res, next) => {  
-
+exports.signup = (req, res, next) => {
+    const user = req.body
+    connect.query('INSERT INTO customers', user, function (error, result) {
+        if (error) {
+            console.log(error);
+            return res.status(400).json({ error })
+        };
+        return res.status(201).json ({ message : 'Utilisateur créé.'})
+    });
 };
 
 exports.login = (req, res, next) => {
