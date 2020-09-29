7624c677bd98d131b7b6cef41d8b914cc446504e
Gautier-c <gautier.couturier@gmail.com>
2020-09-29 18:16:37 +0200

    ajout controllers message modify et delete answer

--- BackendGroupomania/controllers/mesages.js
+++ BackendGroupomania/controllers/mesages.js
@@ -71,11 +71,48 @@ exports.answer = (req, res, next) => {
 };
 
 exports.modifyAnswer = (req, res, next) => {
-
+    connect.query('SELECT * FROM answer WHERE idANSWER=?', req.params.id, function(error,results){
+        if (error){
+            return res.status(400).json({ error })
+        }
+        const token = req.headers.authorisation.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'pGQ6IkWDhhns7Qzqb52dsHFNJYLfZ5NO');
+        const userId = decodedToken.userId;
+        const role = decodedToken.role
+        const answerId = results[0].idUSERS
+        if (userId !== answerId && role !== 'admin') {
+            return res.status(401).json({ message : 'Impossible de modifier ce message.'})
+        }
+        const modifyAnswer = req.body;
+        connect.query('UPDATE answer SET ? WHERE idANSWER=?',[modifyAnswer, req.params.id], function (error,results){
+            if (error){
+                return res.status(400).json({ error })
+            }
+            return res.status(200).json({ message : 'Message modifié.'})
+        })
+    })
 };
 
 exports.deleteAnswer = (req, res, next) => {
-
+    connect.query('SELECT * FROM answer WHERE idANSWER=?', req.params.id, function(error,results){
+        if (error) {
+            return res.status(400).json({ error })
+        }
+        const token = req.headers.authorisation.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'pGQ6IkWDhhns7Qzqb52dsHFNJYLfZ5NO');
+        const userId = decodedToken.userId;
+        const role = decodedToken.role
+        const answerId = results[0].idUSERS
+        if (userId !== answerId && role !== 'admin') {
+            return res.status(401).json({ message : 'Impossible de supprimer ce message.'})
+        }
+        connect.query(`DELETE FROM answer WHERE idANSWER=${req.params.id}`, req.params.id, function (error,results){
+            if (error){
+                return res.status(400).json({ error })
+            }
+            return res.status(200).json({ message : 'Message supprimé.'})
+        })
+    })
 };
 
 exports.likeMessages = (req, res, next) => {
