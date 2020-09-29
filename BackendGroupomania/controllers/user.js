bed4c0bc8c39efc17c3506fe4937d9309d9a35b8
Gautier-c <gautier.couturier@gmail.com>
2020-09-29 18:38:01 +0200

    ajout bcrypt controller user

--- BackendGroupomania/controllers/user.js
+++ BackendGroupomania/controllers/user.js
@@ -1,14 +1,19 @@
 const connect = require('../mysqlDbConnect');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
-    const user = req.body
-    connect.query('INSERT INTO customers', user, function (error, result) {
-        if (error) {
-            console.log(error);
-            return res.status(400).json({ error })
-        };
-        return res.status(201).json ({ message : 'Utilisateur créé.'})
-    });
+    const user = req.body;
+    bcrypt.hash(user.password, 10)
+    .then(hash => {
+        connect.query('INSERT INTO customers', user, function (error, result) {
+            if (error) {
+                console.log(error);
+                return res.status(400).json({ error })
+            };
+            return res.status(201).json ({ message : 'Utilisateur créé.'})
+        });
+    });  
 };
 
 exports.login = (req, res, next) => {
