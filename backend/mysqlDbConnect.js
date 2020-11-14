a1056e51299cf39ac0900116293f91c2581a63c0
Gautier-c <gautier.couturier@gmail.com>
2020-11-14 17:04:15 +0100

    changement nom fichier .env, ajout axios

--- backend/mysqlDbConnect.js
+++ backend/mysqlDbConnect.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv').config();
 const mysql = require('mysql');
 
 const conDb = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 module.exports = conDb;
